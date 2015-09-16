3578f603aba4676efeb266a935625d0d95c7df49
Lawrance Fung <fungjl@wustl.edu>
2015-09-16 15:35:21 -0500

    Initialize project and database

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Lawrance\Dropbox\Coding Projects\Website\fungsite\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
